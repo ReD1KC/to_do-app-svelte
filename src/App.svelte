d111108b2809b6bcadf633ba3e1011ebb64d4a31
Konstantin Chirkov <tchirkokwork@gmail.com>
2024-11-08 11:40:27 +0700

    Init

---
+++ src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+    import svelteLogo from "./assets/svelte.svg";
+    import viteLogo from "/vite.svg";
+    import Counter from "./lib/Counter.svelte";
+    import TaskList from "./components/TaskList.svelte";
+</script>
+
+<main>
+    <div class="container">
+        <h1>Трекер задач</h1>
+    </div>
+    <div class="tasklist">
+        !!!
+        <TaskList/>
+    </div>
+</main>
+
+<style>
+    .container {
+        text-align: center;
+    }
+
+    .tasklist {
+        text-align: center;
+        padding-top: 30px;
+    }
+</style>
